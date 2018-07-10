cf33bdd888c19f0137d33365d8689f2022970d1d
Quentin Perret <quentin.perret@arm.com>
2018-07-10 15:44:09 +0100

    ANDROID: cpufreq/schedutil: Select frequency using util_avg for RT
    Schedutil always requests max frequency whenever a RT task is running.
    Now that we have a better estimate of the utilization of RT runqueues,
    it is possible to make a less conservative decision and scale frequency
    according to the needs of the RT tasks.
    
    To do so, protect the RT-go-to-max code with a new sched_feature.
    The sched_feature is disabled by default, hence favoring energy savings
    as required in mobile environments.
    
    Signed-off-by: Quentin Perret <quentin.perret@arm.com>
    Change-Id: Ic9f01c8703d4f843addaa0d684012a422fe9f3b8
    Signed-off-by: PrimoDev23 <lexx.ps2711@gmail.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -143,3 +143,8 @@ SCHED_FEAT(SCHEDTUNE_BOOST_HOLD_ALL, false)
  * If disabled, boosts will only bias tasks to higher-capacity CPUs.
  */
 SCHED_FEAT(SCHEDTUNE_BOOST_UTIL, true)
+
+/*
+ * Request max frequency from schedutil whenever a RT task is running.
+ */
+SCHED_FEAT(SUGOV_RT_MAX_FREQ, false)
